637a10257af6167b2b4fb701b179d9280faff4e4
Adam Shannon <adamkshannon@gmail.com>
2018-02-23 17:12:15 -0600

    move structs over to main

--- main.go
+++ main.go
@@ -24,6 +24,53 @@ var (
 	flagDirectory = flag.String("directory", "./examples/", "Directory of files to parse")
 )
 
+// Generated with github.com/gnewton/chidley
+//
+// chidley -X after2 >> main.go
+
+type ChiChidleyRoot314159 struct {
+	Chiplist *Chiplist `xml:"plist,omitempty" json:"plist,omitempty"`   // ZZmaxLength=0
+}
+
+type Chiplist struct {
+	Attrversion string`xml:"version,attr"  json:",omitempty"`  // maxLength=3
+	Chidict []*Chidict `xml:"dict,omitempty" json:"dict,omitempty"`   // ZZmaxLength=0
+}
+
+type Chidict struct {
+	Chiarray *Chiarray `xml:"array,omitempty" json:"array,omitempty"`   // ZZmaxLength=0
+	Chidata []*Chidata `xml:"data,omitempty" json:"data,omitempty"`   // ZZmaxLength=0
+	Chidate *Chidate `xml:"date,omitempty" json:"date,omitempty"`   // ZZmaxLength=0
+	Chidict []*Chidict `xml:"dict,omitempty" json:"dict,omitempty"`   // ZZmaxLength=0
+	Chiinteger []*Chiinteger `xml:"integer,omitempty" json:"integer,omitempty"`   // ZZmaxLength=0
+	Chikey []*Chikey `xml:"key,omitempty" json:"key,omitempty"`   // ZZmaxLength=0
+	Chistring *Chistring `xml:"string,omitempty" json:"string,omitempty"`   // ZZmaxLength=0
+}
+
+type Chikey struct {
+	Text string `xml:",chardata" json:",omitempty"`   // maxLength=40
+}
+
+type Chidata struct {
+	Text string `xml:",chardata" json:",omitempty"`   // maxLength=440
+}
+
+type Chidate struct {
+	Text string `xml:",chardata" json:",omitempty"`   // maxLength=20
+}
+
+type Chiarray struct {
+	Chidict []*Chidict `xml:"dict,omitempty" json:"dict,omitempty"`   // ZZmaxLength=0
+}
+
+type Chiinteger struct {
+	Text string `xml:",chardata" json:",omitempty"`   // maxLength=11
+}
+
+type Chistring struct {
+	Text string `xml:",chardata" json:",omitempty"`   // maxLength=17
+}
+
 func main() {
 	fds, err := ioutil.ReadDir(*flagDirectory)
 	if err != nil {
